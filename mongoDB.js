574ec0d3adee4204f41184fa6c319811c02d1e7d
sofiaRubie <sofiarubie9928@gmail.com>
2024-11-28 21:26:23 -0600

    Final Backend with MongoDB
    Authors and publishers

---
+++ mongoDB.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin:12345@library.1hxit.mongodb.net/library?retryWrites=true&w=majority&appName=library', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch((error) => console.error('Error conectando a MongoDB:', error));
+
+module.exports = mongoose;
+
